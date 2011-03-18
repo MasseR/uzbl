6b25bdb806582c35a0447738c740d99267ee1a12
Mats Rauhala <mats.rauhala@gmail.com>
2011-03-18 22:38:49 +0200

    Faster url

--- examples/data/scripts/createhistoryweights.hs
+++ examples/data/scripts/createhistoryweights.hs
@@ -1,10 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
 import qualified Data.ByteString.Lazy.Char8 as B8
 import Data.List
-import qualified Data.Map as M
 import System.Environment.XDG.BaseDir
 
 sigmoid x = 1 / (exp (-x) + 1)
-
 
 main :: IO ()
 main = do
@@ -17,8 +16,8 @@ main = do
       weights = map (\x -> (fromIntegral (length x), last x)) grouped
       maxweight = maximum $ map fst weights
       total = map (updateWeights maxweight) weights
-      rbmap = M.fromList $ total
-  B8.writeFile whistoryfile $ B8.pack $ show rbmap
+  B8.writeFile whistoryfile $ B8.unlines $ map format total
   where
+    format (url, weight) = (B8.pack . show) weight `B8.append` (' ' `B8.cons` url)
     updateWeights maxweight (weight, (url, oldweight)) =
       (url, (sigmoid (weight / maxweight)) * oldweight)
--- examples/data/scripts/load_url_from_history.hs
+++ examples/data/scripts/load_url_from_history.hs
@@ -1,13 +1,19 @@
-import System.Process
 import qualified Data.ByteString.Lazy.Char8 as B8
 import System.Environment.XDG.BaseDir
 import qualified Data.Map as M
 import Data.List
+import System.Environment
+
+calculatedHistory :: IO (M.Map B8.ByteString Float)
+calculatedHistory = do
+  whistoryfile <- getUserDataFile "uzbl" "whistory"
+  h <- (map (B8.words) . B8.lines) `fmap` B8.readFile whistoryfile
+  return $ foldr (\[w, u] m -> M.insertWith' (\_ _ -> undefined) u ((read . B8.unpack) w) m) M.empty h
 
 main = do
-  whistoryfile <- getUserDataFile "uzbl" "whistory"
+  weighted <- calculatedHistory
   historyfile <- getUserDataFile "uzbl" "history"
   history <- (map (head . drop 2 . B8.words) . B8.lines) `fmap` B8.readFile historyfile
-  weighted <- read `fmap` readFile whistoryfile :: IO (M.Map B8.ByteString Float)
-  let updatedmap = foldl' (\m u -> M.insertWith' (flip const) u 1 m) weighted history
-  mapM_ B8.putStrLn $ map fst $ sortBy (\(_,a) (_,b) -> a `compare` b) $ M.assocs updatedmap
+  let updatedmap = foldr (\u m -> M.insertWith' (flip const) u 0.5 m) weighted history
+      urls = map fst $ sortBy (\(_,a) (_,b) -> b `compare` a) $ M.assocs updatedmap
+  B8.putStr $ B8.unlines urls
